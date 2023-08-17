1ec48c563b657860e49e89c4c2d6156bcc790ead
Rafael Koch Peres <rafaelkperes@gmail.com>
2023-08-17 08:54:23 -0300

    Change to lazyvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,1 +1,2 @@
-require("rkperes")
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,11 @@
+return {
+  { "rose-pine/neovim", name = "rose-pine" },
+  { "ellisonleao/gruvbox.nvim" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      -- colorscheme = "rose-pine-main",
+      colorscheme = "gruvbox",
+    },
+  },
+}
