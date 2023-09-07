7945db3409da36a275d207ead1f5109d4d0675f4
Rafael Koch Peres <rafaelkperes@gmail.com>
2023-09-07 09:45:25 -0300

    Update nvim config

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,3 +1,17 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- rust
+opts = function(_, opts)
+  local cmp = require("cmp")
+  opts.sources = cmp.config.sources(vim.list_extend(opts.sources, {
+    { name = "crates" },
+  }))
+end
+
+opts = function(_, opts)
+  if type(opts.ensure_installed) == "table" then
+    vim.list_extend(opts.ensure_installed, { "ron", "rust", "toml" })
+  end
+end
