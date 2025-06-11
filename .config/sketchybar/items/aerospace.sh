97228dbb44b83ec4139cb82fd19d219ace458462
Rafael Koch Peres <rafaelkperes@gmail.com>
2025-06-11 09:20:10 -0300

    Sketchybar with monitors and empty spaces

--- .config/sketchybar/items/aerospace.sh
+++ .config/sketchybar/items/aerospace.sh
@@ -1,17 +1,33 @@
 #!/usr/bin/env bash
 
 sketchybar --add event aerospace_workspace_change
-for sid in $(aerospace list-workspaces --all); do
-	echo "$CONFIG_DIR/plugins/aerospacer.sh $sid"
-    sketchybar --add item space."$sid" left \
-        --subscribe space."$sid" aerospace_workspace_change \
-        --set space."$sid" \
-        background.color=0x44ffffff \
-        background.corner_radius=5 \
-        background.height=20 \
-        background.drawing=off \
+_EMPTY=$(aerospace list-workspaces --monitor all --empty)
+for mid in $(aerospace list-monitors --format "%{monitor-id}"); do
+    sketchybar --add item monitor."$mid" left \
+        --set monitor."$mid" \
+        background.color=0x22ffffff \
+        background.corner_radius=0 \
+        background.drawing=on \
         label.font.size=10.0 \
-        label="$sid" \
-        click_script="aerospace workspace $sid" \
-		script="$CONFIG_DIR/plugins/aerospacer.sh $sid"
+        label="󰍹 $mid" \
+
+        for sid in $(aerospace list-workspaces --monitor $mid); do
+                echo "$CONFIG_DIR/plugins/aerospacer.sh $sid"
+                _IS_EMPTY=$(echo "$_EMPTY" | grep -w "$sid")
+                _COLOR=$([[ -z "$_IS_EMPTY" ]] && echo "0xffffffff" || echo "0x44ffffff" )
+                sketchybar --add item space."$sid" left \
+                        --subscribe space."$sid" aerospace_workspace_change \
+                        --set space."$sid" \
+                        background.color=0x44ffffff \
+                        background.corner_radius=2 \
+                        background.height=20 \
+                        background.drawing=off \
+                        label.font.size=10.0 \
+                        label.padding_left=0 \
+                        label.padding_right=5 \
+                        label.color=$_COLOR \
+                        label="$sid" \
+                        click_script="aerospace workspace $sid" \
+                        script="$CONFIG_DIR/plugins/aerospacer.sh $sid"
+        done
 done
